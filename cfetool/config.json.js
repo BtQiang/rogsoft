bf8f0eeace6a3e0ce3c4def92382d7a2e9decb32
sadoneli <sadoneli@gmail.com>
2021-02-10 13:34:29 +0800

    update cfetool

--- cfetool/config.json.js
+++ cfetool/config.json.js
@@ -1,11 +1,11 @@
 {
-"version":"2.5",
-"md5":"3957f075a33795f1a2b8cf27a4b14e60",
+"version":"2.6",
+"md5":"f36cb7c5578006114fa0f1d8f3e27117",
 "home_url":"Module_cfetool.asp",
 "title":"CFE工具箱",
 "description":"CFE工具箱，查看CFE信息，改机器为国区",
 "changelog":"",
-"build_date":"2021-02-08_21:37:59",
+"build_date":"2021-02-10_13:34:16",
 "server":"42.192.18.234",
 "port":"8083"
 }
